2d3bf784397b32a0535463a7dca18537ee5368e2
Briankim254 <kimutaibrian560@gmail.com>
2023-06-15 20:55:46 +0300

    first commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Brian`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Data Scientist", "API Engineer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>Welcome to my personal portfolio website! I'm passionate about web development, data science, and API development. With a strong background in these fields, I strive to create innovative and user-friendly digital experiences. Whether it's crafting elegant and responsive websites, crunching data to uncover valuable insights, or building robust APIs to connect applications, I'm dedicated to delivering high-quality solutions that meet and exceed expectations. Take a tour of my portfolio and explore the projects I've worked on, showcasing my expertise and creativity. Let's create something amazing together!</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
